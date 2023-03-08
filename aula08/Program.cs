f6fd915c23d3b1332b2e132cc06ad448a2d1c479
ProjetoOliveira <100374048+ProjetoOliveira@users.noreply.github.com>
2023-03-08 14:53:32 -0300

    Add files via upload

---
+++ aula08/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace aula08;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Console.WriteLine("Hello, World!");
+        //Console.ReadKey();
+
+        //Instanciar um objeto a partir de uma classe
+        Exemplo exe01 = new Exemplo();
+
+        exe01.mostramsg();
+
+        exe01.mostranome("Çabi");
+
+        Console.WriteLine(exe01.msg());
+
+        Console.WriteLine("A soma é: " + exe01.somar(10, 5));
+    }
+}
