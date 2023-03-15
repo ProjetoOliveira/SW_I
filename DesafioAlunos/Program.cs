795e8a5fbdf5408887eda4b4a64711f8c6760463
ProjetoOliveira <100374048+ProjetoOliveira@users.noreply.github.com>
2023-03-15 15:32:52 -0300

    Add files via upload

---
+++ DesafioAlunos/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace DesafioAlunos;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Alunos aluno1 = new Alunos();
+
+        Console.Write("Nome do aluno(a): ");
+        string? nome = Console.ReadLine();
+        Console.Write("Digite a nota: ");
+        double nota1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("A nota digitada foi: "+ nota1);
+        Console.Write("Digite a segunda nota: ");
+        double nota2 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("A nota digitada foi: "+ nota2);
+        aluno1.nome = nome;
+        aluno1.nota1 = nota1;
+        aluno1.nota2 = nota2;
+
+        aluno1.mensagem();
+    }
+}
