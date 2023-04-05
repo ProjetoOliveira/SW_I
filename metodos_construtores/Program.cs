875c115997f63bfb4fd5ef9e6315222e7c6c4235
ProjetoOliveira <100374048+ProjetoOliveira@users.noreply.github.com>
2023-04-05 14:26:14 -0300

    Add files via upload

---
+++ metodos_construtores/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace metodos_construtores;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Pessoa p1 = new Pessoa();
+        Pessoa p1 = new Pessoa("Heloísa",16);
+        //p1.insereNome("Heloísa");
+        //p1.insereSobreNome("Oliveira");
+        //p1.insereIdade(16);
+        p1.mostraDados();
+    }
+}
